a3580f399f82256ec2a40388daf3e25dd76658e4
Manuela Köpfli <koepflma@uw.edu>
2023-06-27 14:58:47 -0700

    scatseisnet plots script

--- code/scatenet.py
+++ code/scatenet.py
@@ -3,20 +3,24 @@
 import os
 import pickle
 
-import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 import numpy as np
 import obspy
 from obspy.clients.fdsn.client import Client 
-import sklearn
-from sklearn.decomposition import FastICA
+import time
 
 from scatseisnet import ScatteringNetwork
-%config InlineBackend.figure_format = "svg"
+
+dirpath_save = "../example_long"
+
+# Create directory to save the results
+os.makedirs(dirpath_save, exist_ok=True)
+
+print('Package imported and folder created.')
 
 # load data ===============================================================================================
-
+stime = time.time() # start time
 client = Client("IRIS")
 
 # Collect waveforms from the datacenter
@@ -25,8 +29,8 @@ stream = client.get_waveforms(
     station="SHW",
     location="*",
     channel="EHZ",
-    starttime=obspy.UTCDateTime("2004-09-22T00:00"),
-    endtime=obspy.UTCDateTime("2004-09-25T23:00.1"),
+    starttime=obspy.UTCDateTime("2004-09-01T00:00"),
+    endtime=obspy.UTCDateTime("2004-10-01T00:00.1"),
 )
 
 stream.detrend("linear")
@@ -37,12 +41,8 @@ stream.filter(type="highpass", freq=1.0)
 stream.resample(100)
 stream.plot(rasterized=True);
 
-dirpath_save = "../example_long"
-
-# Create directory to save the results
-os.makedirs(dirpath_save, exist_ok=True)
-
-stream.write("../example_long/scattering_stream.mseed", format="MSEED")
+# stream.write("../example_long/scattering_stream.mseed", format="MSEED")
+print('Loading data tooks {} seconds.'.format(round(time.time()-stime,3)))
 
 # create layers ===============================================================================================
 
@@ -52,7 +52,6 @@ samples_per_segment = int(segment_duration_seconds * sampling_rate_hertz)
 # the network will have 2 layers
 bank_keyword_arguments = (
     {"octaves": 4, "resolution": 4, "quality": 1},
-    {"octaves": 5, "resolution": 2, "quality": 3},
     {"octaves": 5, "resolution": 2, "quality": 3},
 )
 
@@ -87,7 +86,7 @@ for traces in stream.slide(segment_duration, segment_duration * overlap):
     segments.append(np.array([trace.data[:-1] for trace in traces]))
     
 # scattering transform ===============================================================================================
-
+st = time.time() # start time
 scattering_coefficients = network.transform(segments, reduce_type=np.mean)
 
 # Save the features in a pickle file
@@ -95,6 +94,7 @@ np.savez(
     "../example_long/scattering_coefficients.npz",
     order_1=scattering_coefficients[0],
     order_2=scattering_coefficients[1],
-    order_3=scattering_coefficients[2],
     times=timestamps,
 )
+print('Scattering transform and saving took {} seconds.'.format(round(time.time()-st),3))
+print('Script run for {} minutes.'.format(round((time.time()-stime)/60,3)))
